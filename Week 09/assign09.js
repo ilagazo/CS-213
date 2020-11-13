0a63c15e94ced4c5196bc760cf96079bc85cee3d
Ivanro Lagazo <57332846+ilagazo@users.noreply.github.com>
2020-11-13 01:10:50 -0700

    Part 2 and 1 complete. Css is needed now

--- Week 09/assign09.js
+++ Week 09/assign09.js
@@ -1,4 +1,4 @@
-function loadDoc(str) {
+function getCountry(str) {
     var xhttp = new XMLHttpRequest();
     // var regExp = /^[a-zA-Z]/
     if (str === "") {
@@ -7,10 +7,8 @@ function loadDoc(str) {
     }
 
     xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            if (this.status === 200 || this.status === 0) {
-                document.getElementById("country").innerHTML = xhttp.responseText;
-            }
+        if (this.readyState === 4 && this.status === 200) {
+            document.getElementById("country").innerHTML = xhttp.responseText;
         }
     };
     xhttp.open("GET", str, true);
@@ -19,16 +17,37 @@ function loadDoc(str) {
 
 function loadFilename() {
     var xmlhttp = new XMLHttpRequest();
+    var myObj, i, x = "";
     var url = document.getElementById("url").value;
+    var table = "<tr><th>First Name</th><th>Last Name</th><th>Address</th><th>Major</th><th>G.P.A.</th></tr>";
     console.log(url);
 
-    xmlhttp.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            var myObj = JSON.parse(this.responseText);
-            document.getElementById("students").innerHTML = myObj.first;
-        }
-    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            myObj = JSON.parse(this.responseText);
+            x = table;
 
+            // FOR Loop to access key/values in JSON OBJ after parsing
+            for (i in myObj.students) {
+                // Name Section of JSON
+                x += "<tr><td>" + myObj.students[i].first + "</td>";
+                x += "<td>" + myObj.students[i].last + "</td>";
+
+                // Address Section of JSON 
+                x += "<td>" + myObj.students[i].address.city + ", ";
+                x += myObj.students[i].address.state + ", ";
+                x += myObj.students[i].address.zip + "</td>";
+
+                // Major & GPA Section of JSON
+                x += "<td>" + myObj.students[i].major + "</td>";
+                x += "<td>" + myObj.students[i].gpa + "</td></tr>";
+            }
+            document.getElementById("students").innerHTML = x;
+        } else {
+            // Error Message for if file not found
+            document.getElementById("students").innerHTML = "ERROR: File was not found. Please check your filename and try again!"
+        }
+    };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
