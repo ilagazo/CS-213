2acb552f7832dfcc3ba8af17795f048acc55042d
Ivanro Lagazo <57332846+ilagazo@users.noreply.github.com>
2020-12-12 23:22:09 -0700

    Finished CSS and moving to PHP

--- Week 13/assign13.js
+++ Week 13/assign13.js
@@ -13,20 +13,20 @@ function hide() {
     document.getElementById("hidden_div").style.display="none";
 }
 
-function display() {
-    var pType = document.getElementsByName("performance").value;
-    var fName = document.getElementsByName("first_name").value;
-    var lName = document.getElementsByName("last_name").value;
-    var id = document.getElementsByName("student_id").value;
-    var fName_2 = document.getElementsByName("first_name_2").value;
-    var lName_2 = document.getElementsByName("last_name_2").value;
-    var id_2 = document.getElementsByName("student_id_2").value;
-    var skill = document.getElementsByName("skill").value;
-    var instrument = document.getElementsByName("instrument").value;
-    var location = document.getElementsByName("location").value;
-    var roomNum = document.getElementsByName("room").value;
-    var time = document.getElementsByName("time_slot").value;
-}
+// function display() {
+//     var pType = document.getElementsByName("performance").value;
+//     var fName = document.getElementsByName("first_name").value;
+//     var lName = document.getElementsByName("last_name").value;
+//     var id = document.getElementsByName("student_id").value;
+//     var fName_2 = document.getElementsByName("first_name_2").value;
+//     var lName_2 = document.getElementsByName("last_name_2").value;
+//     var id_2 = document.getElementsByName("student_id_2").value;
+//     var skill = document.getElementsByName("skill").value;
+//     var instrument = document.getElementsByName("instrument").value;
+//     var location = document.getElementsByName("location").value;
+//     var roomNum = document.getElementsByName("room").value;
+//     var time = document.getElementsByName("time_slot").value;
+// }
 
 // function validate() {
 //     validatePerformance();
